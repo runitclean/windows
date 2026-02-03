be1391530152321a318ba4b621bf019c28eea63a
RadioNoiseE <rne.kou@icloud.com>
2026-02-03 12:16:44 +0800

    eliminate global state

--- global.h
+++ global.h
@@ -6,9 +6,3 @@
 #include <string.h>
 #include <wayland-client.h>
 #include <wayland-util.h>
-
-struct state {
-  struct ext_image_copy_capture_manager_v1 *image_copy_manager;
-  struct ext_foreign_toplevel_image_capture_source_manager_v1
-      *toplevel_source_manager;
-};
