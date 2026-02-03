a42b3e9a122acbe7d1190a58c987ba7e57060ef1
RadioNoiseE <rne.kou@icloud.com>
2026-02-03 12:16:22 +0800

    image copy capture owns its state

--- image_copy.c
+++ image_copy.c
@@ -1,56 +1,60 @@
 #include "image_copy.h"
 
-static void ext_image_copy_capture_frame_handle_ready (
-    void *data, struct ext_image_copy_capture_frame_v1 *frame) {
-  struct image_copy_frame *f = data;
-  f->ready                   = true;
+static void
+image_copy_capture_frame_ready (void                                   *data,
+                                struct ext_image_copy_capture_frame_v1 *frame) {
+  struct image_copy_frame *icf = data;
+  icf->ready                   = true;
 }
 
-static void ext_image_copy_capture_frame_handle_failed (
-    void *data, struct ext_image_copy_capture_frame_v1 *frame,
-    uint32_t reason) {
-  struct image_copy_frame *f = data;
-  f->failed                  = true;
+static void
+image_copy_capture_frame_failed (void                                   *data,
+                                 struct ext_image_copy_capture_frame_v1 *frame,
+                                 uint32_t reason) {
+  struct image_copy_frame *icf = data;
+  icf->failed                  = true;
 }
 
-static const struct ext_image_copy_capture_frame_v1_listener frame_listener = {
-    .ready  = ext_image_copy_capture_frame_handle_ready,
-    .failed = ext_image_copy_capture_frame_handle_failed,
+static const struct ext_image_copy_capture_frame_v1_listener
+    image_copy_capture_frame_listener = {
+        .ready  = image_copy_capture_frame_ready,
+        .failed = image_copy_capture_frame_failed,
 };
 
 struct image_copy_frame *
-image_copy_from_toplevel (struct state                          *s,
+image_copy_from_toplevel (struct image_copy                     *ic,
                           struct ext_foreign_toplevel_handle_v1 *handle) {
-  struct image_copy_frame *f = calloc (1, sizeof (*f));
+  struct image_copy_frame *icf = calloc (1, sizeof (*icf));
 
   struct ext_image_capture_source_v1 *source =
       ext_foreign_toplevel_image_capture_source_manager_v1_create_source (
-          s->toplevel_source_manager, handle);
+          ic->toplevel_source_manager, handle);
 
-  f->session = ext_image_copy_capture_manager_v1_create_session (
-      s->image_copy_manager, source, false);
-  f->frame = ext_image_copy_capture_session_v1_create_frame (f->session);
+  icf->session = ext_image_copy_capture_manager_v1_create_session (
+      ic->image_copy_manager, source, false);
+  icf->frame = ext_image_copy_capture_session_v1_create_frame (icf->session);
 
-  ext_image_copy_capture_frame_v1_add_listener (f->frame, &frame_listener, f);
+  ext_image_copy_capture_frame_v1_add_listener (
+      icf->frame, &image_copy_capture_frame_listener, icf);
   ext_image_capture_source_v1_destroy (source);
 
-  return f;
+  return icf;
 }
 
-void image_copy_init (struct image_copy_frame *f, struct wl_buffer *buffer) {
-  f->buffer = buffer;
-  f->ready  = false;
-  f->failed = false;
+void image_copy_init (struct image_copy_frame *icf, struct wl_buffer *buffer) {
+  icf->buffer = buffer;
+  icf->ready  = false;
+  icf->failed = false;
 
-  ext_image_copy_capture_frame_v1_attach_buffer (f->frame, buffer);
-  ext_image_copy_capture_frame_v1_damage_buffer (f->frame, 0, 0, INT32_MAX,
+  ext_image_copy_capture_frame_v1_attach_buffer (icf->frame, buffer);
+  ext_image_copy_capture_frame_v1_damage_buffer (icf->frame, 0, 0, INT32_MAX,
                                                  INT32_MAX);
-  ext_image_copy_capture_frame_v1_capture (f->frame);
+  ext_image_copy_capture_frame_v1_capture (icf->frame);
 }
 
-void image_copy_destroy (struct image_copy_frame *f) {
-  ext_image_copy_capture_frame_v1_destroy (f->frame);
-  ext_image_copy_capture_session_v1_destroy (f->session);
+void image_copy_destroy (struct image_copy_frame *icf) {
+  ext_image_copy_capture_frame_v1_destroy (icf->frame);
+  ext_image_copy_capture_session_v1_destroy (icf->session);
 
-  free (f);
+  free (icf);
 }
--- image_copy.h
+++ image_copy.h
@@ -5,6 +5,12 @@
 #include "ext-image-copy-capture-v1-protocol.h"
 #include "global.h"
 
+struct image_copy {
+  struct ext_image_copy_capture_manager_v1 *image_copy_manager;
+  struct ext_foreign_toplevel_image_capture_source_manager_v1
+      *toplevel_source_manager;
+};
+
 struct image_copy_frame {
   struct ext_image_copy_capture_frame_v1   *frame;
   struct ext_image_copy_capture_session_v1 *session;
@@ -15,8 +21,8 @@ struct image_copy_frame {
 };
 
 struct image_copy_frame *
-image_copy_from_toplevel (struct state                          *s,
+image_copy_from_toplevel (struct image_copy                     *ic,
                           struct ext_foreign_toplevel_handle_v1 *handle);
 
-void image_copy_init (struct image_copy_frame *f, struct wl_buffer *buffer);
-void image_copy_destroy (struct image_copy_frame *f);
+void image_copy_init (struct image_copy_frame *icf, struct wl_buffer *buffer);
+void image_copy_destroy (struct image_copy_frame *icf);
