fb6a5d81ac452d31246364c77da48597dbdcf8ca
RadioNoiseE <rne.kou@icloud.com>
2026-02-06 11:02:52 +0800

    update required header files

--- global.h
+++ global.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <fcntl.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <time.h>
+#include <unistd.h>
 #include <wayland-client.h>
 #include <wayland-util.h>
