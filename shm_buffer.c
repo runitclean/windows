ef94efc8d9ccde8d8bb7ee6154474338ae1c7a28
RadioNoiseE <rne.kou@icloud.com>
2026-02-20 22:56:54 +0800

    unify initializer and destructor interface

--- shm_buffer.c
+++ shm_buffer.c
@@ -56,19 +56,22 @@ void shm_buffer_registry_global_remove (void               *data,
                                         struct wl_registry *registry,
                                         uint32_t            name) {}
 
-struct shm_buffer *shm_buffer_init (struct wl_shm     *shm,
-                                    enum wl_shm_format format, int32_t width,
-                                    int32_t height, int32_t stride) {
+void shm_buffer_init (struct shm_buffer *sb, struct wl_shm *shm,
+                      enum wl_shm_format format, int32_t width, int32_t height,
+                      int32_t stride) {
   size_t size = height * stride;
 
   int fd = shm_buffer_create (size);
-  if (fd == -1)
-    return NULL;
+  if (fd == -1) {
+    sb = NULL;
+    return;
+  }
 
   void *data = mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (data == MAP_FAILED) {
     close (fd);
-    return NULL;
+    sb = NULL;
+    return;
   }
 
   struct wl_shm_pool *pool = wl_shm_create_pool (shm, fd, size);
@@ -78,23 +81,18 @@ struct shm_buffer *shm_buffer_init (struct wl_shm     *shm,
   wl_shm_pool_destroy (pool);
   close (fd);
 
-  struct shm_buffer *sb = calloc (1, sizeof (struct shm_buffer));
-
   sb->buffer = buffer;
   sb->data   = data;
   sb->width  = width;
   sb->height = height;
   sb->stride = stride;
   sb->format = format;
-
-  return sb;
 }
 
-void shm_buffer_destory (struct shm_buffer *sb) {
+void shm_buffer_destroy (struct shm_buffer *sb) {
   if (sb == NULL)
     return;
 
   munmap (sb->data, sb->height * sb->stride);
   wl_buffer_destroy (sb->buffer);
-  free (sb);
 }
--- shm_buffer.h
+++ shm_buffer.h
@@ -18,7 +18,7 @@ void shm_buffer_registry_global_remove (void               *data,
                                         struct wl_registry *registry,
                                         uint32_t            name);
 
-struct shm_buffer *shm_buffer_init (struct wl_shm     *shm,
-                                    enum wl_shm_format format, int32_t width,
-                                    int32_t height, int32_t stride);
-void               shm_buffer_destory (struct shm_buffer *sb);
+void shm_buffer_init (struct shm_buffer *sb, struct wl_shm *shm,
+                      enum wl_shm_format format, int32_t width, int32_t height,
+                      int32_t stride);
+void shm_buffer_destroy (struct shm_buffer *sb);
