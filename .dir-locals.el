9b210efa834644cfe61757409531c93e5160a69b
RadioNoiseE <rne.kou@icloud.com>
2026-02-20 12:34:38 +0800

    remove so-called unsafe dir local

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,2 @@
-((nil . ((eglot-ignored-server-capabilities . ':inlayHintProvider)))
- (c-mode . ((c-file-style . "GNU")))
+((c-mode . ((c-file-style . "GNU")))
  (c-ts-mode . ((c-ts-mode-indent-style . gnu))))
