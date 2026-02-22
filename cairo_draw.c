024d222fc33589771f5b5577deeb8e4fe3dd935e
RadioNoiseE <rne.kou@icloud.com>
2026-02-22 16:31:55 +0800

    cleanup cairo draw module

--- cairo_draw.c
+++ cairo_draw.c
@@ -17,33 +17,37 @@ void cairo_draw_destroy (struct cairo_draw *cd) {
 
 void cairo_draw_window (struct cairo_draw *cd, void *data, int32_t width,
                         int32_t height, int32_t stride, int32_t x, int32_t y,
-                        float scale_factor, bool focused) {
+                        float scale, bool focused) {
   cairo_surface_t *surface = cairo_image_surface_create_for_data (
     data, CAIRO_FORMAT_ARGB32, width, height, stride);
 
   cairo_save (cd->context);
 
   cairo_translate (cd->context, x, y);
-  cairo_scale (cd->context, scale_factor, scale_factor);
+  cairo_scale (cd->context, scale, scale);
 
   cairo_set_source_surface (cd->context, surface, 0, 0);
   cairo_paint (cd->context);
 
   if (focused) {
     cairo_set_source_rgba (cd->context, 1, 1, 1, 1);
-    cairo_set_line_width (cd->context, 1 / scale_factor);
-    cairo_rectangle (cd->context, -1 / scale_factor, -1 / scale_factor,
-                     width + 2 / scale_factor, height + 2 / scale_factor);
+    cairo_set_line_width (cd->context, 1 / scale);
+
+    cairo_rectangle (cd->context, -1 / scale, -1 / scale, width + 2 / scale,
+                     height + 2 / scale);
     cairo_stroke (cd->context);
   }
 
   cairo_restore (cd->context);
+
   cairo_surface_destroy (surface);
 }
 
 void cairo_draw_clear (struct cairo_draw *cd) {
   cairo_save (cd->context);
+
   cairo_set_operator (cd->context, CAIRO_OPERATOR_CLEAR);
   cairo_paint (cd->context);
+
   cairo_restore (cd->context);
 }
--- cairo_draw.h
+++ cairo_draw.h
@@ -13,5 +13,5 @@ void cairo_draw_destroy (struct cairo_draw *cd);
 
 void cairo_draw_window (struct cairo_draw *cd, void *data, int32_t width,
                         int32_t height, int32_t stride, int32_t x, int32_t y,
-                        float scale_factor, bool focused);
+                        float scale, bool focused);
 void cairo_draw_clear (struct cairo_draw *cd);
